2c27aa5db8bb80299705e32adf2c812866bfe1bc
Vadym Chernov <119622651+VadymChornyUkraine@users.noreply.github.com>
2023-05-09 15:52:48 +0300

    Add files via upload

--- a.py
+++ a.py
@@ -1,5 +1,6 @@
 import hickle as hkl
 import numpy as np
+from RALf1FiltrVID import filterFourierQ
 arrrxx=hkl.load("ralfrez.rlf2")
 # i=0 
 # arrrxx_=[]
@@ -9,15 +10,16 @@ arrrxx=hkl.load("ralfrez.rlf2")
 #     i=i+1
 #hkl.dump(arrrxx_,"ralfrez.rlf2")
 bbb=np.asarray(arrrxx,float).transpose()
-%varexp --plot bbb
+%varexp --plot bbb 
 len(arrrxx)
 
+lSrez=0.99
 dNIt=8
 Ngroup=3
 Lo=1
 Nproc=3*Ngroup#*(os.cpu_count())
-wrkdir = r"C:/Users/VadymChornyy/Desktop/Work/WX15/"
-[hhhao,Arr_AAA]=(hkl.load(wrkdir + "BTC-USD"+".rlf1"))
+wrkdir = r"C:/Work/WX16/"
+[hhhao,Arr_AAA]=(hkl.load(wrkdir + "ADA-USD"+".rlf1"))
 NIter=100
 
 for hhhai in range(hhhao):  
@@ -31,37 +33,30 @@ for hhhai in range(hhhao):
     ZDat=xxxx.copy()        
     
     anI=len(ZDat)
-    # for i in range(anI):  
-    #     if Lo:
-    #         ZDat[i][:len(ar0)]=np.log(ar0)
-    #     else:
-    #         ZDat[i][:len(ar0)]=ar0.copy()
-    
-    if Lo:
-        ar0x=np.exp(np.median(ZDat,axis=0))  
-        ar0x_=.04*np.median(abs(ZDat-np.log(ar0x)),axis=0)
-    else:
-        ar0x=np.median(ZDat,axis=0)
-        ar0x_=.04*(np.median(abs((ZDat)-(ar0x)),axis=0))
+   
+    ar0x=np.median(ZDat,axis=0)
+    ar0x_=.4*(np.median(abs((ZDat)-(ar0x)),axis=0))
         
     lnn=len(ZDat[0])
+    NNew=int(.35*lnn)
     for i in range(anI):    
         for j in range(lnn):    
-            if Lo:             
-                if not abs(ZDat[i,j]-np.log(ar0x[j]))<=ar0x_[j]:     
-                    if ZDat[i,j]<(np.log(ar0x[j])-ar0x_[j]):            
-                        ZDat[i,j]=np.log(ar0x[j])-ar0x_[j]
-                    else: 
-                        if ZDat[i,j]>(np.log(ar0x[j])+ar0x_[j]):
-                            ZDat[i,j]=np.log(ar0x[j])+ar0x_[j]
-            else:
-                if not abs(ZDat[i,j]-(ar0x[j]))<=ar0x_[j]:     
-                    if ZDat[i,j]<((ar0x[j])-ar0x_[j]):            
-                        ZDat[i,j]=(ar0x[j])-ar0x_[j]
-                    else:
-                        if ZDat[i,j]>((ar0x[j])+ar0x_[j]):
-                            ZDat[i,j]=(ar0x[j])+ar0x_[j]
-                    
+            if not abs(ZDat[i,j]-(ar0x[j]))<=ar0x_[j]:     
+                if ZDat[i,j]<((ar0x[j])-ar0x_[j]):            
+                    ZDat[i,j]=(ar0x[j])-ar0x_[j]
+                else:
+                    if ZDat[i,j]>((ar0x[j])+ar0x_[j]):
+                        ZDat[i,j]=(ar0x[j])+ar0x_[j]
+    anI=anI
+    P=np.zeros(3,float)
+    for i in range(anI):
+        dd=ZDat[i][lnn-NNew:].copy()                         
+        x=ar0x[lnn-NNew:lnn-NNew+int(lSrez*(NNew-(lnn-len(ar0x))))].copy()
+        ZDat[i][lnn-NNew:]=filterFourierQ(ZDat[i],(ar0x),NNew,1)[lnn-NNew:]
+        P[0:2]=np.polyfit(x,ZDat[i][lnn-NNew:lnn-NNew+int(lSrez*(NNew-(lnn-len(ar0x))))],1)
+        if not P[0]>0:
+            P[0:2]=np.polyfit(dd,ZDat[i][lnn-NNew:],1)
+        ZDat[i][lnn-NNew:]=(ZDat[i][lnn-NNew:]-P[1])/P[0]                      
     #ZDat=Arr_AAA[iGr*NIter*int(Nproc/Ngroup)+max(0,(hhh+1)-dNIt)*int(Nproc/Ngroup):iGr*NIter*int(Nproc/Ngroup)+(hhh+1)*int(Nproc/Ngroup)].copy()
     bbbbb=ZDat[:,:].transpose().copy()
     aaaaa=np.median(bbbbb.transpose(),axis=0)
